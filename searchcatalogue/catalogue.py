9eec44805af7adeb876661b59a44fe1312c3f96e
PUNARVA VYAS <punarvavyas97@gmail.com>
2020-03-25 14:54:30 -0300

    changes
    FOLDER DOCKER FILE

---
+++ searchcatalogue/catalogue.py
@@ -0,0 +1,10 @@
+
+
+def cataloguee(pr1_name):
+    f=open("catalogue.txt",'a+')
+    f.write(pr1_name)
+    f.close()
+
+
+
+
---
+++ searchcatalogue/main.py
@@ -0,0 +1,35 @@
+from flask import Flask,request
+from flask_cors import CORS
+import mongo as m
+import catalogue as c
+import searchlog as sl
+import time
+from bson.json_util import dumps
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+note_dict=dict()
+pr_name = ""
+
+@app.route('/mongodbcall',methods=['GET','POST'])
+def mongodbcall():
+
+    pr_name = request.form.get('str1user')
+    print(pr_name)
+    t1=time.time()
+    sl.searchlogg(pr_name,t1)
+    x=m.mongo_ext(pr_name)
+    #c.cataloguee(x)
+
+
+    return x
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='3.82.157.77', port=5000, debug=False)
